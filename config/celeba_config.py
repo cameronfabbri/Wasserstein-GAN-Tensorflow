dc15ec9e951980334113a8a9eac5b5801e1d0abf
Cameron Fabbri <cameronfabbri@gmail.com>
2017-02-24 23:20:33 -0600

    update

---
+++ config/celeba_config.py
@@ -0,0 +1,6 @@
+checkpoint_dir = 'checkpoints/'
+learning_rate  = 5e-5
+batch_size     = 64
+dataset        = 'celeba'
+load           = True # True/False, load all images into RAM or not
+num_critic     = 5 # number of critics
